af068715fca6ad32d2c80ca1e6f3b401332e4541
张淼 <zhangmiao5518@gmail.com>
2018-08-13 20:44:44 +0800

    Runloop 初体验

---
+++ RunLoop/代码/001--RunLoop初体验/001--RunLoop初体验/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  001--RunLoop初体验
+//
+//  Created by H on 17/2/9.
+//  Copyright © 2017年 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
