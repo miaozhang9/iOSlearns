c900b0f42db1d4c161456c342524f447cc32ff48
张淼 <zhangmiao5518@gmail.com>
2018-08-20 22:22:39 +0800

    runtime 初体验

---
+++ RunLoop/代码/runtime体验/runtime体验/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  runtime体验
+//
+//  Created by H on 17/2/9.
+//  Copyright © 2017年 TZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        Person * p = [[Person alloc]init];
+        
+    }
+    return 0;
+}
