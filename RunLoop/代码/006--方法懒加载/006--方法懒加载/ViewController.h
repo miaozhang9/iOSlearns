868db185308f7c19a92efa48881f28aaeae81458
张淼 <zhangmiao5518@gmail.com>
2018-08-26 16:55:04 +0800

    方法懒加载

---
+++ RunLoop/代码/006--方法懒加载/006--方法懒加载/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  006--方法懒加载
+//
+//  Created by H on 17/2/9.
+//  Copyright © 2017年 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunLoop/代码/006--方法懒加载/006--方法懒加载Tests/_06_______Tests.m
@@ -0,0 +1,39 @@
+//
+//  _06_______Tests.m
+//  006--方法懒加载Tests
+//
+//  Created by H on 17/2/9.
+//  Copyright © 2017年 TZ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _06_______Tests : XCTestCase
+
+@end
+
+@implementation _06_______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
